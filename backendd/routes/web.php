6488372d9379d6f60d04b640e77192bb5309c4cc
Axel Hernández <axeelhrz@gmail.com>
2025-08-11 18:16:54 -0300

    Checkpoint for message d55fc8db-3294-43b1-b504-6699701af7e9

--- backendd/routes/web.php
+++ backendd/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Add CSRF cookie route for SPA
+Route::get('/sanctum/csrf-cookie', function () {
+    return response()->json(['message' => 'CSRF cookie set']);
+})->middleware('web');
