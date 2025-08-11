db3fee0a04f3170190d72a912398ee553f1d1465
Axel Hernández <axeelhrz@gmail.com>
2025-08-11 08:37:18 -0300

    Checkpoint for message b820e578-dd18-4b08-bd5b-c4b3eb413907

--- backendd/database/migrations/2025_08_11_113446_create_leagues_table.php
+++ backendd/database/migrations/2025_08_11_113446_create_leagues_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('leagues', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('region')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
+            
+            $table->index(['status']);
         });
     }
 
