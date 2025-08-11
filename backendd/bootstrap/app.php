2138ddfecbefcbf79db8b13d706164562e87395b
Axel Hernández <axeelhrz@gmail.com>
2025-08-11 18:17:23 -0300

    Checkpoint for message 279e346e-4e73-43ff-8cfc-d57613b4f0b9

--- backendd/bootstrap/app.php
+++ backendd/bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->validateCsrfTokens(except: [
             'stripe/*',
+            'api/auth/*', // Temporarily disable CSRF for auth routes
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
