65b602ef8080f25684f2baa27e84e89f2c85cc36
Axel Hernández <axeelhrz@gmail.com>
2025-08-12 14:57:53 -0300

    Checkpoint for message 124647a0-2c63-42c8-86c1-b5e6e0c771be

--- src/components/asociacion/index.ts
+++
@@ -1,6 +0,0 @@
-export { SocioProfileView } from './SocioProfileView';
-export { EnhancedMemberManagement } from './EnhancedMemberManagement';
-export { SociosTable } from './SociosTable';
-export { EnhancedSocioDialog } from './EnhancedSocioDialog';
-export { CsvImport } from './CsvImport';
-export { EnhancedCsvImport } from './EnhancedCsvImport';
