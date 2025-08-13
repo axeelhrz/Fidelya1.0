f95333859aef0e2a345204801ac8c852b37d7156
Axel Hernández <axeelhrz@gmail.com>
2025-08-13 12:48:58 -0300

    Checkpoint for message 15739283-95e7-4e15-8adc-005eeeffe073

--- backendd/config/sanctum.php
+++ backendd/config/sanctum.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:3000,127.0.0.1:3000,localhost,127.0.0.1,a34e7fb8521c.ngrok-free.app')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:3000,127.0.0.1:3000,localhost,127.0.0.1,a34e7fb8521c.ngrok-free.app,straight-truly-continuity-glow.trycloudflare.com')),
 
     /*
     |--------------------------------------------------------------------------
