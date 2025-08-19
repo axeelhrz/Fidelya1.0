9cc358c29acefef9b5a6a8a71d6f07a9455a3a8f
Axel Hernández <axeelhrz@gmail.com>
2025-08-18 22:06:30 -0300

    Checkpoint for message bfe15cc6-ef4b-437f-a87f-8b6801a37997

--- frontendd/src/app/sports/[id]/parameters/page.tsx
+++ frontendd/src/app/sports/[id]/parameters/page.tsx
@@ -1,7 +1,7 @@
 import SportParametersClient from './SportParametersClient';
 
 // Generate static params for static export
-export async function generateStaticParams() {
+export function generateStaticParams() {
   // For static export, we need to provide all possible sport IDs at build time
   // Since sports are dynamic, we'll return an empty array for now
   // You can modify this to include actual sport IDs when you have them
